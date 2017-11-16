7beea33257a2669e9fb94eee603875c936cfe273
meckstss <meckstss@gmail.com>
2017-11-15 23:54:06 -0500

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  #get 'demo/index'
+  get 'demo/index'
 
-  get ':controller(/:action(/:id))'
+  #default routing may go away...
+  #get ':controller(/:action(/:id))'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
