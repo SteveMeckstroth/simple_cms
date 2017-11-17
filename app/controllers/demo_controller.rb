978e6ed380a9731d7400632835bb4edad1f03b71
Meckstroth Steve - Cincinnati-AMIG <n1300705@am.munichre.com>
2017-11-17 13:15:01 -0500

    redirect

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,7 +1,16 @@
 class DemoController < ApplicationController
 
- layout false  
+ layout false
 
   def index
+    render('hello')
+  end
+
+  def hello
+    render('index')
+  end
+
+  def other_hello
+    redirect_to(:controller => 'demo', :action => 'index')
   end
 end
